dd80a63e85a8cd5f6e072f4a13f8bb62a2c4992c
therealMallow <alexisberrios.a@gmail.com>
2025-05-18 20:40:57 -0300

    Initial commit

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite[] faces;
+    public Sprite cardBack;
+
+    public int cardIndex;
+
+    public void ToggleFace(bool showFace){
+        if(showFace){
+            // Show front side
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else {
+            //Show backside
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
